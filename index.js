3e4b1e41b9e114659f4a7d21fa49c45d0f1a0e03
Muhammad Sulton <musulton@gmail.com>
2023-04-11 13:41:38 +0700

    typing event

--- index.js
+++ index.js
@@ -13,8 +13,21 @@ io.on('connection', (socket) => {
   console.log('a user connected');
 
   socket.on('chat message', (msg) => {
+    /*
+  Perbedaan io dan socket
+    1. Socket -> digunakan ketika kita ingin mengirim acara ke user dia / socket sendiri
+    2. Io -> digunakan ketika kita ingin mengirim acara ke semua user / socket
+    3. Socket.broadcast -> digunakan ketika kita ingin mengirim acara ke semua orang kecuali socket / dia sendiri
+  */
+ 
     io.emit('chat message', msg);
+    // socket.emit('chat message, msg);
+    // socket.broadcast.emit('chat message', msg);
   });
+
+  socket.on('typing', (typing) => {
+    io.emit('typing', typing);
+  })
 
   socket.on('disconnect', () => {
     console.log('user disconnected');
